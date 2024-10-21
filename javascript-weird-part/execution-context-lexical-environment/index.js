4d56e79a87593a0707ff6fec1031cec18f7d8149
jeffmlb91 <66559059+jeffmlb91@users.noreply.github.com>
2024-10-20 23:12:45 -0400

    hoisting

---
+++ javascript-weird-part/execution-context-lexical-environment/index.js
@@ -0,0 +1,17 @@
+// const a = "We are learning";
+
+// function test() {
+//      console.log(a)
+// }
+
+// console.log(test());
+
+b()
+console.log(a);
+
+let a = "Hi there!";
+
+function b() {
+    console.log("B is called!")
+}
+
